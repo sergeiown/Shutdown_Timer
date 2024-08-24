c593e83ac64c47c084908784d125bcd0819dddb3
Serhii Myshko <sergeiown@gmail.com>
2024-08-24 10:08:11 +0300

    add script for creating a shortcut in the start menu

---
+++ startmenu_shortcut.bat
@@ -0,0 +1,18 @@
+@echo off
+set shortcutName=Shutdown Timer
+set shortcutTargetDescription=Run Shutdown Timer
+set targetPath=%CD%\shutdown_timer.bat
+set folderPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs
+set shortcutPath=%folderPath%\%shortcutName%.lnk
+set iconPath=%SystemRoot%\System32\SHELL32.dll,27
+set workingDirectory=%CD%
+
+if exist "%shortcutPath%" (
+    del "%shortcutPath%"
+)
+
+echo Creating the shortcut %shortcutPath% & echo.
+powershell -Command "$WScript=New-Object -ComObject WScript.Shell; $Shortcut=$WScript.CreateShortcut('%shortcutPath%'); $Shortcut.TargetPath='%targetPath%'; $Shortcut.IconLocation='%iconPath%'; $Shortcut.WorkingDirectory='%workingDirectory%'; $Shortcut.Description='%shortcutTargetDescription%'; $Shortcut.WindowStyle=1; $Shortcut.Save()"
+
+
+echo The operation is complete. & echo. & pause
